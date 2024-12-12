bcd74e904828428b72b0d70614564a468733eef3
Guilherme Bolincenha <gbolincenha@gmail.com>
2024-12-12 11:33:43 -0300

    Traduzindo meu App no Frappe Framework #18

--- library_management/library_management/doctype/library_member/library_member.js
+++ library_management/library_management/doctype/library_member/library_member.js
@@ -3,12 +3,12 @@
 
 frappe.ui.form.on("Library Member", {
 	refresh(frm) {
-        frm.add_custom_button('Create Membership', () => {
+        frm.add_custom_button(__('Create Membership'), () => {
             frappe.new_doc('Library Membership', {
                 library_member: frm.doc.name
             })
         })
-        frm.add_custom_button('Create Transaction', () => {
+        frm.add_custom_button(__('Create Transaction'), () => {
             frappe.new_doc('Library Transaction', {
                 library_member: frm.doc.name
             })
