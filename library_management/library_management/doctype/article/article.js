9abd96ed71ff6620c41221a93f9c50a4515ae89b
Guilherme Bolincenha <gbolincenha@gmail.com>
2025-01-27 14:54:10 -0300

    Frappe Ajax Call #23

--- library_management/library_management/doctype/article/article.js
+++ library_management/library_management/doctype/article/article.js
@@ -1,8 +1,19 @@
 // Copyright (c) 2024, Guilherme Bolincenha and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Article", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Article", {
+	refresh(frm) {
+		if(frm.doc.status=="Issued"){
+			frm.add_custom_button(__('Book Holder'), () => {
+				frm.call("get_book_holder")
+					.then(r => {
+						if(r){
+							frappe.msgprint(r.message)
+						}else{
+							frappe.msgprint(__("No holder found"))
+						}
+					})
+			})
+		}
+	},
+});
--- library_management/library_management/doctype/article/article.py
+++ library_management/library_management/doctype/article/article.py
@@ -1,9 +1,32 @@
 # Copyright (c) 2024, Guilherme Bolincenha and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.website.website_generator import WebsiteGenerator
-
+from frappe import _
 
 class Article(WebsiteGenerator):
-	pass
+	@frappe.whitelist()
+	def get_book_holder(self):
+		if self.status != "Issued":
+			frappe.throw(_("Article is not issued"))
+
+		last = frappe.get_all("Library Transaction",
+			filters={
+				"article": self.name,
+				"docstatus": 1
+			},
+			fields=["type", "library_member"],
+			order_by="creation desc",
+			limit=1,
+		)
+		if not last:
+			return None
+
+		if last[0].type != "Issue":
+			return None
+
+		if not frappe.db.exists("Library Member", last[0].library_member):
+			frappe.throw(_("Library Member not found"))
+
+		return frappe.get_value("Library Member", last[0].library_member, "full_name")
