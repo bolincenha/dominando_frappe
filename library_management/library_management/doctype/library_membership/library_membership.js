8932c8db97906dcce57e6e5651e7dc268c3afad7
Guilherme Bolincenha <gbolincenha@gmail.com>
2025-02-06 10:31:44 -0300

    Substituindo a Consulta de Link por um Script Personalizado #31

--- library_management/library_management/doctype/library_membership/library_membership.js
+++ library_management/library_management/doctype/library_membership/library_membership.js
@@ -2,6 +2,15 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on("Library Membership", {
+	setup(frm) {
+        frm.set_query("library_member", function() {
+            return {
+                filters: {
+                    "enabled": 1,
+                }
+            };
+        });
+	},
 	from_date(frm) {
         if(frm.doc.from_date){
             frm.call('set_membership_period')
