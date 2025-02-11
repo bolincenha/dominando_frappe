3e994612417a1abe5681c9e31729ff82a242a8db
Guilherme Bolincenha <gbolincenha@gmail.com>
2025-02-11 15:51:37 -0300

    Adicionando Botões ao Formulário #33

--- library_management/library_management/doctype/library_member/library_member.js
+++ library_management/library_management/doctype/library_member/library_member.js
@@ -3,31 +3,38 @@
 
 frappe.ui.form.on("Library Member", {
 	refresh(frm) {
-        frm.add_custom_button(__('Create Membership'), () => {
-            frappe.new_doc('Library Membership', {
-                library_member: frm.doc.name
-            })
-        })
-        frm.add_custom_button(__('Create Transaction'), () => {
-            frappe.new_doc('Library Transaction', {
-                library_member: frm.doc.name
-            })
-        })
         frm.add_custom_button(__('Add Phone'), () => {
             frm.add_child('phone', {
                 phone: ''
             })
             frm.refresh_field('phone');
         })
-        frm.add_custom_button(__('Set Full Name'), () => {
-            frm.set_value('full_name', `${frm.doc.first_name} ${frm.doc.last_name}`)
-        })
+        frm.change_custom_button_type(__('Add Phone'), null, 'primary');
+        frm.add_custom_button(__('Create Membership'), () => {
+            frappe.new_doc('Library Membership', {
+                library_member: frm.doc.name
+            })
+        }, __("Actions"))
         frm.add_custom_button(__('Clear Phones'), () => {
             frm.doc.phone.forEach((row) => {
                 frappe.model.set_value(row.doctype, row.name, 'phone', '');
             })
-        })
+        }, __("Actions"))
+        frm.add_custom_button(__('Create Transaction'), () => {
+            frappe.new_doc('Library Transaction', {
+                library_member: frm.doc.name
+            })
+        }, __("Actions"))
+        frm.add_custom_button(__('Set Full Name'), () => {
+            frm.set_value('full_name', `${frm.doc.first_name} ${frm.doc.last_name}`)
+        }, __("Actions"))
 	},
+    enable(frm){
+        frm.set_value('enabled', 1);
+    },
+    disable(frm){
+        frm.set_value('enabled', 0);
+    }
 });
 
 frappe.ui.form.on("Library Member Phone", {
