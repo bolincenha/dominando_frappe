e7ae8d275765b5d062e2c0a7957cac0f4056e064
Guilherme Bolincenha <gbolincenha@gmail.com>
2025-02-14 17:05:23 -0300

    Set Intro, Is New e Is Dirty Form Scripts #35

--- library_management/library_management/doctype/article/article.js
+++ library_management/library_management/doctype/article/article.js
@@ -1,8 +1,12 @@
 // Copyright (c) 2024, Guilherme Bolincenha and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Article", {
-// 	refresh(frm) {
-		
-// 	},
-// });
+frappe.ui.form.on("Article", {
+	refresh(frm) {
+        if (frm.is_new()) {
+            if (!frm.doc.description) {
+                frm.set_intro('Please set the value of description', 'blue');
+            }
+        }
+	},
+});
